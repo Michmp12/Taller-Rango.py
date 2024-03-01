e2e5c941fee78746e970b779aea45c989f949c2c
MichMP <158522214+Michmp12@users.noreply.github.com>
2024-03-01 11:39:09 -0500

    Taller-Rango.py

--- Rango.py
+++ Rango.py
@@ -1,9 +1,9 @@
-var_numeroInt = int(input('ingrese por favor la cantidad de numeros que desea evaluar para realizar el rango'))
-contador = 0
-
-for i in range(var_numeroInt):
-    num = float(input(f"ingrse el numero {i + 1}: "))
-    if num >= 10 and num <=20:
-        contador += 1
-        
-print(f'la cantidad de numeros en el rango de 10 a 20 es: {contador}')
+cantidadInt = int(input('¿Cantidad de números?'))
+contadorInt = 1
+contadornumerosInt = 0
+while contadorInt  <= cantidadInt:
+    numeroInt = int(input('Ingrese un número -> '))
+    if numeroInt >= 10 and numeroInt <=20:
+        contadornumerosInt += 1
+    contadorInt += 1 
+print('La cantidad de números en el rango de 10 a 20 son: ', contadornumerosInt)
